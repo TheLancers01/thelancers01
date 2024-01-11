41dc0180e8ce868dd1640332dae305a6e361f7df
carlwall123 <carlwall123@gmail.com>
2024-01-10 20:49:04 -0600

    update

--- project/src/main/java/thelancers01/project/models/data/WorkoutRepository00Cesar.java
+++
@@ -1,9 +0,0 @@
-package thelancers01.project.models.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import thelancers01.project.models.Workoutb;
-
-@Repository
-public interface WorkoutRepository00Cesar extends CrudRepository<Workoutb, Integer> {
-}
