e2e066a60e432ab214149f93043d86b94874e6d7
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-01-17 09:49:18 -0600

    Personal Records Updated

--- project/src/main/java/thelancers01/project/models/data/ExerciseRepository.java
+++ project/src/main/java/thelancers01/project/models/data/ExerciseRepository.java
@@ -3,6 +3,7 @@ package thelancers01.project.models.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import thelancers01.project.models.Exercise;
+import thelancers01.project.models.User;
 
 import java.util.List;
 
